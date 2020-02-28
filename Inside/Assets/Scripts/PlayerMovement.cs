68ea2984793407858de899bc83b96218104eba75
MrMarexo <mr.marexo@gmail.com>
2020-02-28 20:21:57 +0100

    Jump

--- Inside/Assets/Scripts/PlayerMovement.cs
+++ Inside/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runSpeed = 4f;
     [SerializeField] float jumpForce = 3f;
 
-    BoxCollider2D col;
+    CapsuleCollider2D col;
     LayerMask mask;
 
     Rigidbody2D rb;
@@ -15,19 +15,58 @@ public class PlayerMovement : MonoBehaviour
 
     int horizontal = 0;
 
+    Vector2 curVelocity;
+    Vector2 newVelocity;
+
+    ContactFilter2D contFilter;
+    RaycastHit2D[] hits = new RaycastHit2D[10];
+
+    bool isGrounded;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<BoxCollider2D>();
+        col = GetComponent<CapsuleCollider2D>();
         anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+
         CalcHorizontal();
-        Run();
+        isGrounded = CollideTest(Vector2.down);
+
+        //calc movement
+        curVelocity = rb.velocity;
+        newVelocity = new Vector2(horizontal * runSpeed, curVelocity.y);
+        if (Mathf.Abs(rb.velocity.x) > 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            newVelocity.y = jumpForce;
+            anim.SetTrigger("isJumping");
+        }
+
+        if (horizontal == -1 && CollideTest(Vector2.left))
+        {
+            newVelocity.x = 0;
+        }
+
+        if (horizontal == 1 && CollideTest(Vector2.right))
+        {
+            newVelocity.x = 0;
+        }
+
+        rb.velocity = newVelocity;
 
     }
 
@@ -52,17 +91,19 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void Run()
+
+    bool CollideTest(Vector2 direction)
     {
-        var velocity = new Vector2(horizontal * runSpeed, rb.velocity.y);
-        rb.velocity = velocity;
-        if (Mathf.Abs(rb.velocity.x) > 0)
+        float dist = 0.1f;
+
+
+        if (rb.Cast(direction, contFilter, hits, dist) > 0)
         {
-            anim.SetBool("isRunning", true);
+            return true;
         }
         else
         {
-            anim.SetBool("isRunning", false);
+            return false;
         }
     }
 

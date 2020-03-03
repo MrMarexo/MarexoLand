bc2ea1fc62305bb7d1dec4e5acb9749ee49c9902
MrMarexo <mr.marexo@gmail.com>
2020-03-03 21:02:15 +0100

    Moving platforms

---
+++ Inside/Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    [SerializeField] Transform startPos;
+    [SerializeField] Transform pos1;
+    [SerializeField] Transform pos2;
+
+    Transform nextPos;
+    [SerializeField] float moveSpeed = 10f;
+
+    private void Start()
+    {
+        nextPos = pos2;
+        transform.position = startPos.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position == pos1.position)
+        {
+            nextPos = pos2;
+        }
+        else if (transform.position == pos2.position)
+        {
+            nextPos = pos1;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos.position, moveSpeed * Time.fixedDeltaTime);
+    }
+}
